85efe7f787bf2da89d6cdcf5b163379770a5b01c
Mubin <maksudmubin@gmail.com>
2022-06-23 19:18:52 +0600

    # progress bar added in SplashActivity.kt # design fix needed

--- app/src/main/java/com/mubin/appscheduler/ui/SplashActivity.kt
+++ app/src/main/java/com/mubin/appscheduler/ui/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.mubin.appscheduler.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.mubin.appscheduler.databinding.ActivitySplashBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
@@ -15,7 +19,10 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 1500)
 
     }
 }
